368d7a763add1a17e5e9c2f432df711a4092a6b6
Andika6679 <114057440+Andika6679@users.noreply.github.com>
2024-11-17 13:16:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+﻿document.getElementById('downloadBtn').addEventListener('click', function () {
+    window.location.href = 'https://kodular.app/KJT-CMR';  // Ganti dengan link download aplikasi Anda
+});
